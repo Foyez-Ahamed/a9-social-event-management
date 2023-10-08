0d59f8eeca160c81875fd201da2b9b6cff22246c
Foyez-Ahamed <foyez1435@gmail.com>
2023-10-08 21:33:01 +0600

    install firebase on project and added firebase config file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZv2b1yIjV2bHnhzc_EAx6KMF_GN4AamQ",
+  authDomain: "social-events-management-925e1.firebaseapp.com",
+  projectId: "social-events-management-925e1",
+  storageBucket: "social-events-management-925e1.appspot.com",
+  messagingSenderId: "610015791442",
+  appId: "1:610015791442:web:bbddca70b5b2068759928b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

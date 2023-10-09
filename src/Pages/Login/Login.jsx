64790ab31edcf7ed2cc567e2549ecb76596b99c9
Foyez-Ahamed <foyez1435@gmail.com>
2023-10-09 16:36:41 +0600

    project updated by find some mistake and solve it

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -58,16 +58,16 @@ const Login = () => {
 <h1 className="text-center font-medium text-3xl text-[#403F3F] mb-6"> Login Here Please</h1>
 </div>
 
-{/* form start*/}
+
 <form onSubmit={handleLogin}>
 
 <div className="relative">
 <label htmlFor="email">Email address</label> <br /> 
 <input type= "email"
- placeholder="enter your email..." name="email" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[350px] md:w-[390px] lg:w-[390px]" /> <br />
+ placeholder="enter your email..." name="email" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[320px] md:w-[390px] lg:w-[390px]" /> <br />
 
 <label htmlFor="password">Password</label> <br />
-<input type = {showPassIcon ? "text" : "password"} placeholder="enter your password" required name="password" className="mt-4 input bg-[#F3F3F3] w-[350px] md:w-[390px] lg:w-[390px]"/>
+<input type = {showPassIcon ? "text" : "password"} placeholder="enter your password" required name="password" className="mt-4 input bg-[#F3F3F3] w-[320px] md:w-[390px] lg:w-[390px]"/>
  <br />
 
  <span onClick={() => setShowPassIcon(!showPassIcon)} className="cursor-pointer absolute right-[10px] top-[160px]">
@@ -80,16 +80,16 @@ const Login = () => {
 
 <h1 className="mt-3 cursor-pointer text-[#403F3F]">Forgot password ? </h1>
 
- {/* button  */}
-<input type="submit" value= "Login" className=" btn w-[350px] md:w-[390px] lg:w-[390px] mt-5 font-bold bg-[#FF900E] text-white hover:bg-gray-600" />
+ 
+<input type="submit" value= "Login" className=" btn w-[320px] md:w-[390px] lg:w-[390px] mt-5 font-bold bg-[#FF900E] text-white hover:bg-gray-600" />
 </div>
 </form>
 
 <button onClick={() => handleGoogleLogin(providerGoogle)} className= " bg-[#403F3F] flex gap-2 justify-center items-center w-full py-3 rounded-md mt-5 text-white hover:bg-[#FF900E] text-[15px] font-medium uppercase">Login with <FcGoogle className="text-xl"></FcGoogle></button>
-{/* form end */}
 
 
-{/* others content show here please */}
+
+
 <div className="mt-5">
     <p>Do not have an account ? <Link to='/register' className="text-bold text-[#1877F2] text-md uppercase underline">Register</Link></p>
 </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -72,28 +72,28 @@ const Register = () => {
 <h1 className="text-center font-medium text-3xl text-[#403F3F] mb-6">Register Here Please</h1>
 </div>
 
-{/* form start*/}
+
 <form onSubmit={handleRegister}>
 
 <div className="relative">
 
 <label htmlFor="name">Name</label> <br /> 
 <input type= "Text"
- placeholder="enter your name..." name="name" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[350px] md:w-[390px] lg:w-[390px]" /> <br />
+ placeholder="enter your name..." name="name" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[320px] md:w-[390px] lg:w-[390px]" /> <br />
 
  <label htmlFor="photoURL">Photo URL</label> <br /> 
 <input type= "text"
- placeholder="Photo URL..." name="photoURL" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[350px] md:w-[390px] lg:w-[390px]" /> <br />    
+ placeholder="Photo URL..." name="photoURL" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[320px] md:w-[390px] lg:w-[390px]" /> <br />    
 
 <label htmlFor="email">Email address</label> <br /> 
 <input type= "email"
- placeholder="enter your email..." name="email" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[350px] md:w-[390px] lg:w-[390px]" /> <br />
+ placeholder="enter your email..." name="email" required className=" mt-4 mb-4 input bg-[#F3F3F3] w-[320px] md:w-[390px] lg:w-[390px]" /> <br />
 
 <label htmlFor="password">Password</label> <br />
-<input type= {showPassIcon ? "text" : "password"} placeholder="enter your password" required name="password" className="mt-4 input bg-[#F3F3F3] w-[350px] md:w-[390px] lg:w-[390px]"/>
+<input type= {showPassIcon ? "text" : "password"} placeholder="enter your password" required name="password" className="mt-4 input bg-[#F3F3F3] w-[320px] md:w-[390px] lg:w-[390px]"/>
  <br />
 
- <span onClick={() => setShowPassIcon(!showPassIcon)} className="cursor-pointer absolute right-[10px] top-[265px]">
+ <span onClick={() => setShowPassIcon(!showPassIcon)} className="cursor-pointer absolute right-[10px] top-[368px]">
 
  {
     showPassIcon ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
@@ -104,14 +104,14 @@ const Register = () => {
  <input type="checkbox" name="terms" id="terms" className="mt-5" />
  <label htmlFor="terms" className="ml-2">Accepted our <a href="" className="text-[#403F3F]">terms and conditions</a></label> <br />
 
- {/* button  */}
-<input type="submit" value= "Register" className=" btn w-[350px] md:w-[390px] lg:w-[390px] mt-5 font-bold bg-[#FF900E] text-white hover:bg-gray-600" />
+
+<input type="submit" value= "Register" className=" btn w-[320px] md:w-[390px] lg:w-[390px] mt-5 font-bold bg-[#FF900E] text-white hover:bg-gray-600" />
 </div>
 
 </form>
-{/* form end */}
 
-{/* others content show here please */}
+
+
 <div className="mt-5">
     <p> Already have an account ? <Link to='/login' className="text-bold text-[#1877F2] text-md uppercase underline">Login</Link></p>
 </div>
